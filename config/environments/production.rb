3ffbf6617ce8650f9e85da664e87766c0a5d7616
Jake He <zhex900@gmail.com>
2014-10-12 21:44:44 +0800

    Add SMTP config for action mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 Owums::Application.configure do
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'churchinperth.org',
+      :user_name            => 'admin@churchinperth.org',
+      :password             => 'matt.25.23.faithfulslave',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
